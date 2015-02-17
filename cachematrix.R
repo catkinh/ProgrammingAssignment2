16db144f38438a5c6c1271addd3477b6ec78cc1a
catkinh <chazzatkins2007@hotmail.com>
2015-02-17 17:40:33 +0000

    Unannotated Version
    Working script in need of annotation

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedmatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    cachedmatrix <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) cachedmatrix <<- matrix
+  getmatrix <- function() cachedmatrix
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invmatrix <- x$getmatrix()
+  if(!is.null(invmatrix)) {
+    message("getting matrix inverse from cache")
+    return(invmatrix)
+  }
+  data <- x$get()
+  invmatrix <- solve(data, ...)
+  x$setmatrix(invmatrix)
+  invmatrix
 }
-
-
-## TEST
